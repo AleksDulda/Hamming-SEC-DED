dafbeec1e316d42d8a5aadd4259f42100b2b3693
Aleks <139750907+AleksDulda@users.noreply.github.com>
2025-06-07 17:58:43 +0300

    Add files via upload

---
+++ Docs/Codes/BLM230_Proje_AleksDulda_21360859025/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace HammingSECDED
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button encodeButton;
+        private System.Windows.Forms.ListBox memoryListBox;
+        private System.Windows.Forms.TextBox errorPosTextBox;
+        private System.Windows.Forms.Button errorButton;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.TextBox generalParityTextBox;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            inputTextBox = new TextBox();
+            encodeButton = new Button();
+            memoryListBox = new ListBox();
+            errorPosTextBox = new TextBox();
+            errorButton = new Button();
+            checkButton = new Button();
+            outputLabel = new Label();
+            generalParityTextBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            inputTextBox.Location = new Point(73, 136);
+            inputTextBox.Name = "inputTextBox";
+            inputTextBox.Size = new Size(170, 23);
+            inputTextBox.TabIndex = 0;
+            // 
+            // encodeButton
+            // 
+            encodeButton.Location = new Point(264, 111);
+            encodeButton.Name = "encodeButton";
+            encodeButton.Size = new Size(85, 25);
+            encodeButton.TabIndex = 1;
+            encodeButton.Text = "Kodla";
+            // 
+            // memoryListBox
+            // 
+            memoryListBox.ItemHeight = 15;
+            memoryListBox.Location = new Point(376, 58);
+            memoryListBox.Name = "memoryListBox";
+            memoryListBox.Size = new Size(274, 154);
+            memoryListBox.TabIndex = 2;
+            // 
+            // errorPosTextBox
+            // 
+            errorPosTextBox.Location = new Point(731, 119);
+            errorPosTextBox.Name = "errorPosTextBox";
+            errorPosTextBox.PlaceholderText = "Hata Pozisyonu";
+            errorPosTextBox.Size = new Size(89, 23);
+            errorPosTextBox.TabIndex = 3;
+            // 
+            // errorButton
+            // 
+            errorButton.Location = new Point(731, 148);
+            errorButton.Name = "errorButton";
+            errorButton.Size = new Size(89, 25);
+            errorButton.TabIndex = 4;
+            errorButton.Text = "Hata Ekle";
+            // 
+            // checkButton
+            // 
+            checkButton.Location = new Point(897, 120);
+            checkButton.Name = "checkButton";
+            checkButton.Size = new Size(85, 25);
+            checkButton.TabIndex = 5;
+            checkButton.Text = "Kontrol Et";
+            // 
+            // outputLabel
+            // 
+            outputLabel.AutoSize = true;
+            outputLabel.Location = new Point(731, 223);
+            outputLabel.Name = "outputLabel";
+            outputLabel.Size = new Size(40, 15);
+            outputLabel.TabIndex = 6;
+            outputLabel.Text = "Çıktı...";
+            // 
+            // generalParityTextBox
+            // 
+            generalParityTextBox.Location = new Point(627, 215);
+            generalParityTextBox.Name = "generalParityTextBox";
+            generalParityTextBox.PlaceholderText = "Genel Parity";
+            generalParityTextBox.ReadOnly = true;
+            generalParityTextBox.Size = new Size(23, 23);
+            generalParityTextBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(73, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Veri Girişi";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(255, 139);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 15);
+            label2.TabIndex = 9;
+            label2.Text = "-------------------->";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(376, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(52, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Memory";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(538, 218);
+            label4.Name = "label4";
+            label4.Size = new Size(83, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Genel Eşlik Biti";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(662, 122);
+            label5.Name = "label5";
+            label5.Size = new Size(65, 15);
+            label5.TabIndex = 12;
+            label5.Text = "---------->";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(731, 101);
+            label6.Name = "label6";
+            label6.Size = new Size(89, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Hata Pozisyonu";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(826, 125);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 15);
+            label7.TabIndex = 14;
+            label7.Text = "---------->";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(731, 208);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 15);
+            label8.TabIndex = 15;
+            label8.Text = "Çıktılar....";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(73, 162);
+            label9.Name = "label9";
+            label9.Size = new Size(26, 15);
+            label9.TabIndex = 16;
+            label9.Text = "LSB";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(217, 162);
+            label10.Name = "label10";
+            label10.Size = new Size(31, 15);
+            label10.TabIndex = 17;
+            label10.Text = "MSB";
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(1064, 365);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(inputTextBox);
+            Controls.Add(encodeButton);
+            Controls.Add(memoryListBox);
+            Controls.Add(errorPosTextBox);
+            Controls.Add(errorButton);
+            Controls.Add(checkButton);
+            Controls.Add(outputLabel);
+            Controls.Add(generalParityTextBox);
+            Name = "Form1";
+            Text = "Hamming SEC-DED Simülatörü";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+    }
+}
---
+++ Docs/Codes/BLM230_Proje_AleksDulda_21360859025/Form1.cs
@@ -0,0 +1,235 @@
+﻿// Form1.cs
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HammingSECDED
+{
+    public partial class Form1 : Form
+    {
+        List<string> memory = new List<string>();
+        List<string> parityBits = new List<string>();
+        List<int> parityPositions = new List<int>();
+        List<int> dataOneBitPositions = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            encodeButton.Click += encodeButton_Click;
+            errorButton.Click += errorButton_Click;
+            checkButton.Click += checkButton_Click;
+        }
+
+        private void encodeButton_Click(object sender, EventArgs e)
+        {
+            string data = inputTextBox.Text.Trim();
+            if (!IsBinary(data))
+            {
+                MessageBox.Show("Lütfen yalnızca 0 ve 1 içeren bir veri giriniz.");
+                return;
+            }
+            if (!IsValidLength(data))
+            {
+                MessageBox.Show("Veri uzunluğu yalnızca 8, 16 veya 32 bit olabilir.");
+                return;
+            }
+            memory.Add(data);
+            string hammingCode = GenerateHammingCode(data);
+            string parityString = string.Join("", GetParityBitValues(hammingCode));
+            string generalParity = CalculateGeneralParity(hammingCode);
+            generalParityTextBox.Text = generalParity;
+            parityBits.Add(parityString);
+            parityPositions = GetParityBitPositions(hammingCode);
+            dataOneBitPositions = GetDataOneBitPositions(hammingCode);
+
+            memoryListBox.Items.Add(hammingCode);
+            outputLabel.Text = "Veri listeye eklendi: " + data +
+                               "\nHamming kodu: " + hammingCode +
+                               "\nParity string: " + parityString +
+                               "\nGenel Parity: " + generalParity +
+                               "\nParity pozisyonları: " + string.Join(", ", parityPositions) +
+                               "\nData '1' bit pozisyonları: " + string.Join(", ", dataOneBitPositions);
+        }
+
+        private void errorButton_Click(object sender, EventArgs e)
+        {
+            int index = memoryListBox.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Lütfen bir veri seçiniz.");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(errorPosTextBox.Text))
+            {
+                // Kullanıcı boş bıraktıysa, hata uygulanmaz
+                MessageBox.Show("Hata uygulanmadı. Pozisyon belirtilmedi.");
+                return;
+            }
+
+            if (!int.TryParse(errorPosTextBox.Text, out int pos) || pos < 1)
+            {
+                MessageBox.Show("Geçerli bir bit pozisyonu giriniz.");
+                return;
+            }
+
+            string selectedCode = memoryListBox.Items[index].ToString();
+            if (pos > selectedCode.Length)
+            {
+                MessageBox.Show("Girilen pozisyon veri uzunluğunu aşıyor.");
+                return;
+            }
+            char[] chars = selectedCode.ToCharArray();
+            chars[pos - 1] = chars[pos - 1] == '0' ? '1' : '0';
+            string erroredCode = new string(chars);
+            memoryListBox.Items[index] = erroredCode;
+            memory[index] = erroredCode;
+            outputLabel.Text = $"Hata uygulandı. Yeni veri: {erroredCode}";
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            int index = memoryListBox.SelectedIndex;
+            if (index == -1)
+            {
+                MessageBox.Show("Lütfen bir veri seçiniz.");
+                return;
+            }
+            string selectedCode = memoryListBox.Items[index].ToString();
+            List<int> ones = new List<int>();
+            for (int i = 0; i < selectedCode.Length; i++)
+            {
+                if (!IsPowerOfTwo(i + 1) && selectedCode[i] == '1')
+                {
+                    ones.Add(i + 1);
+                }
+            }
+            int result = 0;
+            foreach (int pos in ones)
+            {
+                result ^= pos;
+            }
+            List<char> extractedParity = GetParityBitValues(selectedCode);
+            string parityString = string.Join("", extractedParity);
+            int parityVal = 0;
+            for (int i = 0; i < parityString.Length; i++)
+            {
+                if (parityString[i] == '1')
+                {
+                    parityVal ^= (int)Math.Pow(2, i);
+                }
+            }
+            int finalCheck = result ^ parityVal;
+            int oneCount = Convert.ToString(finalCheck, 2).Count(c => c == '1');
+
+            string genelParityYeni = CalculateGeneralParity(selectedCode);
+            string genelParityEski = generalParityTextBox.Text.Trim();
+
+            outputLabel.Text = $"Sendrom (finalCheck): {finalCheck}\n";
+            outputLabel.Text += $"Genel Parity (önceki): {genelParityEski}, (şimdiki): {genelParityYeni}\n";
+
+            if (finalCheck != 0 && genelParityEski == genelParityYeni)
+            {
+                outputLabel.Text += "Çift hata tespit edildi!\nSistem tek hata zannedip yanlış düzeltme yapabilir.\nBu durumda düzeltme yapılmaz.\n";
+                return;
+            }
+
+            if (finalCheck == 0)
+            {
+                outputLabel.Text += "Veride hata yok.";
+            }
+            else if (oneCount == 1)
+            {
+                outputLabel.Text += "Veri doğru, ancak parity bitinde hata olabilir.";
+            }
+            else if (oneCount >= 2)
+            {
+                outputLabel.Text += $"Data hatalı. Hatalı bit pozisyonu: {finalCheck}. DÜZELTİLİYOR...\n";
+                char[] corrected = selectedCode.ToCharArray();
+                int pos = finalCheck - 1;
+                if (pos >= 0 && pos < corrected.Length)
+                    corrected[pos] = corrected[pos] == '0' ? '1' : '0';
+                string correctedString = new string(corrected);
+                memoryListBox.Items[index] = correctedString;
+                memory[index] = correctedString;
+                outputLabel.Text += $"Düzeltilmiş veri: {correctedString}";
+            }
+        }
+
+        private string GenerateHammingCode(string data)
+        {
+            int m = data.Length;
+            int r = 0;
+            while (Math.Pow(2, r) < m + r + 1) r++;
+            int totalLength = m + r;
+            char[] hamming = new char[totalLength + 1];
+            int j = 0;
+            for (int i = 1; i <= totalLength; i++)
+            {
+                if (IsPowerOfTwo(i))
+                    hamming[i] = '0';
+                else
+                    hamming[i] = data[j++];
+            }
+            for (int i = 0; i < r; i++)
+            {
+                int parity = 0;
+                int pos = (int)Math.Pow(2, i);
+                for (int k = 1; k <= totalLength; k++)
+                {
+                    if (((k >> i) & 1) == 1 && hamming[k] == '1')
+                        parity++;
+                }
+                hamming[pos] = (parity % 2 == 0) ? '0' : '1';
+            }
+            return new string(hamming.Skip(1).ToArray());
+        }
+
+        private List<char> GetParityBitValues(string hammingCode)
+        {
+            List<char> values = new List<char>();
+            for (int i = 1; i <= hammingCode.Length; i++)
+            {
+                if (IsPowerOfTwo(i))
+                    values.Add(hammingCode[i - 1]);
+            }
+            return values;
+        }
+
+        private List<int> GetParityBitPositions(string hammingCode)
+        {
+            List<int> positions = new List<int>();
+            for (int i = 1; i <= hammingCode.Length; i++)
+            {
+                if (IsPowerOfTwo(i))
+                    positions.Add(i);
+            }
+            return positions;
+        }
+
+        private List<int> GetDataOneBitPositions(string hammingCode)
+        {
+            List<int> positions = new List<int>();
+            for (int i = 1; i <= hammingCode.Length; i++)
+            {
+                if (!IsPowerOfTwo(i) && hammingCode[i - 1] == '1')
+                    positions.Add(i);
+            }
+            return positions;
+        }
+
+        private string CalculateGeneralParity(string hammingCode)
+        {
+            int ones = hammingCode.Count(c => c == '1');
+            return (ones % 2 == 0) ? "0" : "1";
+        }
+
+        private bool IsPowerOfTwo(int x) => (x & (x - 1)) == 0 && x != 0;
+        private bool IsBinary(string s) => s.All(c => c == '0' || c == '1');
+        private bool IsValidLength(string s) => s.Length == 8 || s.Length == 16 || s.Length == 32;
+
+       
+    }
+
+}
